ff6c73de96a10f9b04a4256e82572ed380c78d67
devansh5 <devkumar5436@gmail.com>
2021-02-07 02:24:54 +0530

    adding the api

--- server.js
+++ server.js
@@ -1,52 +1,70 @@
-const express=require('express')
-const bodyParser=require('body-parser')
-const app=express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
+const MongoClient = require("mongodb").MongoClient;
 
-const MongoClient=require('mongodb').MongoClient
+MongoClient.connect(
+  "",
+  { useUnifiedTopology: true }
+)
+  .then((client) => {
+    const db = client.db("recipe");
+    console.log("connect to database");
+    const recipesCollection = db.collection("recipetest");
+    app.use(express.static("public"));
+    app.set("view engine", "ejs");
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.get("/", (req, res) => {
+      res.render("index.ejs");
+    });
+    function getNextSequence(name) {
+      var ret = recipesCollection.findAndModify({
+        query: { _id: name },
+        update: { $inc: { seq: 1 } },
+        new: true,
+      });
 
-
-MongoClient.connect("<uri>",{useUnifiedTopology:true})
-.then(client=>{
-    const db=client.db('marvels-quotes')
-    console.log('connect to database')
-    const recipesCollection=db.collection('recipes')
-    app.use(express.static('public'))
-    app.set('view engine','ejs')
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({extended:true}))
-    app.get('/',(req,res)=>{
-        res.render('index.ejs')
-    })
-
-    app.get('/recipes',(req,res)=>{
-        recipesCollection.find().toArray()
-        .then(result=>{
-            res.render('recipe',{recipes:result})
-        }).catch(err=>console.error(err))
-    })
-    app.post('/recipes',(req,res)=>{
-        recipesCollection.insertOne(req.body)
-        .then(result=>{
-            recipesCollection.find().toArray()
-            .then(result=>{
-                res.render('recipe',{recipes:result})
-            }).catch(error=>console.error(error))
-        }).catch(err=>{
-            console.error(err)
+      return ret.seq;
+    }
+    app.get("/recipes", (req, res) => {
+      recipesCollection
+        .find()
+        .toArray()
+        .then((result) => {
+          res.send(result);
         })
-    })
-})
-.catch(err=>{
+        .catch((err) => console.error(err));
+    });
+    app.post("/recipes", (req, res) => {
+      let { xmeme_name, caption_url, caption } = req.body;
+      recipesCollection
+        .insertOne({
+          _id: getNextSequence("recipe_id"),
+          xmeme_name: xmeme_name,
+          caption_url: caption_url,
+          caption: caption,
+        })
+        .then((result) => {
+          recipesCollection
+            .find()
+            .toArray()
+            .then((result) => {
+              res.send(result);
+            })
+            .catch((error) => console.error(error));
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+    app.get("/recipes/:id", (req, res) => {});
+  })
+  .catch((err) => {
     console.error(err);
-})
+  });
 
-
-
-
-
-app.listen(3000,()=>{
-    console.log("running")
-})
-
-
+app.listen(3000, () => {
+  console.log("running");
+});
